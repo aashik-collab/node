b7674057482316f98ed40606c1b39209bc5ae8ea
kritish bhattarai <rishi58rishi@gmail.com>
2020-12-24 14:57:57 +0545

    udpate

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ dotenv.config({ path: path.join(__dirname, '/config', '/.env') });
 
 const PORT = process.env.PORT || 5000;
 const MODE = process.env.MODE || 'DEVELOPMENT';
-const MONGODB_URI_DEV = process.env.MONGODB_URI_DEV || 'mongodb://localhost:27017';
-const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
-const MONGODB_URI = MODE === 'PRODUCTION' ? MONGODB_URI_PROD : MONGODB_URI_DEV;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
 // connecting to database
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
