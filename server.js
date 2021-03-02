ddc3afa5aa384d3d765a26a4e111af274f155d10
kritish bhattarai <rishi58rishi@gmail.com>
2021-03-02 11:43:50 +0545

    update

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const app = require('./app');
 dotenv.config({ path: path.join(__dirname, '/config', '/.env') });
 
 const PORT = process.env.PORT || 5000;
-const MODE = process.env.MODE || 'DEVELOPMENT';
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
 // connecting to database
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log(chalk.bold.blue('mongodb connected'));
+   console.log(chalk.bold.blue('mongodb connected'));
 });
 
 // startng development server
 app.listen(PORT, () => {
-    console.log(chalk.bold.yellow('server is running on port ' + PORT));
+   console.log(chalk.bold.yellow('server is running on port ' + PORT));
 });
