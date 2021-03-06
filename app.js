b782dd143779be11647cb12434da5a37378dc399
kritish bhattarai <rishi58rishi@gmail.com>
2021-03-07 01:19:32 +0545

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const userRoutes = require('./components/user/userRoutes');
 const roomRoutes = require('./components/room/roomRoutes');
 const hallRoutes = require('./components/hall/hallRoutes');
 const routes = require('./routes');
+const bookingsRoutes = require('./components/bookings/routes');
 
 // cors
 app.use(cors());
@@ -18,5 +19,6 @@ app.use('/api/users', upload.none(), userRoutes);
 app.use('/api/rooms', upload.none(), roomRoutes);
 app.use('/api/halls', upload.none(), hallRoutes);
 app.use('/api', upload.none(), routes);
+app.use('/api', bookingsRoutes);
 
 module.exports = app;
